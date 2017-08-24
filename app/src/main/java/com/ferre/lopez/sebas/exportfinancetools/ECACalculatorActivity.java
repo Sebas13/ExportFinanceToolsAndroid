be7e755b187ae8ccfa83a8251a9816faf59bfcca
sebastian lopez ferre <sebas_ferre@hotmail.com>
2017-08-24 23:09:45 +0200

    Problems with ECA_Calculator: Do not know how to convert the data from Edit Text to Int

--- app/src/main/java/com/ferre/lopez/sebas/exportfinancetools/ECACalculatorActivity.java
+++ app/src/main/java/com/ferre/lopez/sebas/exportfinancetools/ECACalculatorActivity.java
@@ -3,6 +3,7 @@ package com.ferre.lopez.sebas.exportfinancetools;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 
 public class ECACalculatorActivity extends AppCompatActivity {
 
@@ -11,9 +12,19 @@ public class ECACalculatorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecacalculator);
 
+        EditText a = (EditText)findViewById(R.id.editText);
+        EditText b = (EditText)findViewById(R.id.editText2);
+        EditText c = (EditText)findViewById(R.id.editText3);
 
+        String spanish_content = a.getText().toString();
+        String third_country_content = b.getText().toString();
+        String get_local_cost = c.getText().toString();
 
-
+       
     }
+
+
+
+
 
 }
--- app/src/main/java/com/ferre/lopez/sebas/exportfinancetools/MenuActivity.java
+++ app/src/main/java/com/ferre/lopez/sebas/exportfinancetools/MenuActivity.java
@@ -1,9 +1,10 @@
 package com.ferre.lopez.sebas.exportfinancetools;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
+
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -11,24 +12,20 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        Button buttonECACalculator = (Button)findViewById(R.id.buttonEcaCalculator);
-        Button buttonCIRRRates = (Button)findViewById(R.id.buttonCIRRRates);
-        Button buttonOECDConsensus = (Button) findViewById(R.id.buttonOECDConsensus);
-        //buttonOECDConsensus.setOnClickListener((View.OnClickListener) this);
     }
 
-    public void gotoECACalculator()
+    public void gotoECACalculator(View v)
     {
-        //Rellenar para pasar a la actividad ECA Calculator
+        startActivity(new Intent(this,ECACalculatorActivity.class));
     }
 
-    public void getCIRRRates ()
+    public void getCIRRRates(View v)
     {
-        //Rellenar con el enlace con el cual se obtienen los CIRR Rates
+        startActivity(new Intent(this,ECACalculatorActivity.class));
     }
 
-    public void getOECDConsensus()
+    public void getOECDConsensus(View v)
     {
-        //Rellenar con el enlace para obtener el consenso de la OCDE
+        startActivity(new Intent(this,ECACalculatorActivity.class));
     }
 }
