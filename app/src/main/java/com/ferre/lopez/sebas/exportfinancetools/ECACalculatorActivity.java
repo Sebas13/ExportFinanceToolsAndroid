bca2aabfe7b2760383678f9497039ae69f1e748e
sebastian lopez ferre <sebas_ferre@hotmail.com>
2017-08-25 21:36:00 +0200

    now it calculates

--- app/src/main/java/com/ferre/lopez/sebas/exportfinancetools/ECACalculatorActivity.java
+++ app/src/main/java/com/ferre/lopez/sebas/exportfinancetools/ECACalculatorActivity.java
@@ -1,30 +1,57 @@
 package com.ferre.lopez.sebas.exportfinancetools;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
 
 public class ECACalculatorActivity extends AppCompatActivity {
+
+    int spanish_content = 12;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ecacalculator);
 
-        EditText a = (EditText)findViewById(R.id.editText);
-        EditText b = (EditText)findViewById(R.id.editText2);
-        EditText c = (EditText)findViewById(R.id.editText3);
+       final EditText a = (EditText) findViewById(R.id.editText);
+       final EditText b = (EditText) findViewById(R.id.editText2);
+       EditText c = (EditText) findViewById(R.id.editText3);
 
-        String spanish_content = a.getText().toString();
-        String third_country_content = b.getText().toString();
-        String get_local_cost = c.getText().toString();
+        a.setText(String.valueOf(0));
+        b.setText(String.valueOf(0));
+        c.setText(String.valueOf(0));
 
-       
+       FloatingActionButton button = (FloatingActionButton) findViewById(R.id.fab);
+       button.setOnClickListener(new View.OnClickListener()
+        {
+            public void onClick(View v)
+            {
+                spanish_content = Integer.parseInt(a.getText().toString());
+                calculate(spanish_content,spanish_content);
+            }
+        });
+    }
+
+       public void calculate (int a, int b)
+            {
+
+               int c =  a + b;
+               String d = String.valueOf(c);
+                int duration = Toast.LENGTH_LONG;
+                Context context = getApplicationContext();
+                Toast toast = Toast.makeText(context, d, duration);
+                toast.show();
+             }
+
     }
 
 
 
 
 
-}
