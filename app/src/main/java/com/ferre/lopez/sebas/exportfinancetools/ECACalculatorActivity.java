15e351cec0d884c1af4eb0a0d6939444adfd589d
sebastian lopez ferre <sebas_ferre@hotmail.com>
2017-08-26 10:26:39 +0200

    little changes

--- app/src/main/java/com/ferre/lopez/sebas/exportfinancetools/ECACalculatorActivity.java
+++ app/src/main/java/com/ferre/lopez/sebas/exportfinancetools/ECACalculatorActivity.java
@@ -5,14 +5,15 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 
 public class ECACalculatorActivity extends AppCompatActivity {
 
-    int spanish_content = 12;
+    int spanish_content;
+    int third_country_content;
+    int local_costs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,7 +22,7 @@ public class ECACalculatorActivity extends AppCompatActivity {
 
        final EditText a = (EditText) findViewById(R.id.editText);
        final EditText b = (EditText) findViewById(R.id.editText2);
-       EditText c = (EditText) findViewById(R.id.editText3);
+       final EditText c = (EditText) findViewById(R.id.editText3);
 
         a.setText(String.valueOf(0));
         b.setText(String.valueOf(0));
@@ -33,16 +34,19 @@ public class ECACalculatorActivity extends AppCompatActivity {
             public void onClick(View v)
             {
                 spanish_content = Integer.parseInt(a.getText().toString());
-                calculate(spanish_content,spanish_content);
+                third_country_content = Integer.parseInt(b.getText().toString());
+                local_costs = Integer.parseInt(c.getText().toString());
+
+                calculate(spanish_content,third_country_content,local_costs);
             }
         });
     }
 
-       public void calculate (int a, int b)
+       public void calculate (int spanish_content, int third_country_content, int local_costs)
             {
 
-               int c =  a + b;
-               String d = String.valueOf(c);
+               int result =  spanish_content + third_country_content + local_costs;
+               String d = String.valueOf(result);
                 int duration = Toast.LENGTH_LONG;
                 Context context = getApplicationContext();
                 Toast toast = Toast.makeText(context, d, duration);
